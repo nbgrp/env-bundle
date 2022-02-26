e89defaecdc4143912e010c37eec6f2f6486f49c
Alexander Menshchikov <amenshchikov@gmail.com>
2022-02-26 23:26:56 +0300

    base64_array and base64url_array + Dependabot + audit on pull_request

--- tests/ArrayCastEnvVarProcessorTest.php
+++ tests/ArrayCastEnvVarProcessorTest.php
@@ -31,31 +31,43 @@ final class ArrayCastEnvVarProcessorTest extends TestCase
      */
     public function successProvider(): \Generator
     {
-        yield [
+        yield 'bool_array' => [
             'bool_array',
             [1, 2, 0, -1, 'true', 'false', 'yes', 'no', 'on', 'off', 'any'],
             [true, true, false, true, true, false, true, false, true, false, false],
         ];
 
-        yield [
+        yield 'int_array' => [
             'int_array',
             ['0', '1', '-2.0', '3.5', 0, 1, -2.0, 3.5],
             [0, 1, -2, 3, 0, 1, -2, 3],
         ];
 
-        yield [
+        yield 'float_array' => [
             'float_array',
             [0.0, -0.0, 1.1, -2.0, 3.5, '0.0', '-0.0', '1.1', '-2.0', '3.5'],
             [0.0, 0.0, 1.1, -2.0, 3.5, 0.0, 0.0, 1.1, -2.0, 3.5],
         ];
 
-        yield [
+        yield 'base64_array' => [
+            'base64_array',
+            ['ZW5jb2RlZA==', 'ZW5jb2RlZDI'],
+            ['encoded', 'encoded2'],
+        ];
+
+        yield 'base64url_array' => [
+            'base64url_array',
+            ['PD9lbmM_Pg==', 'PDw_MT8-Pg'],
+            ['<?enc?>', '<<?1?>>'],
+        ];
+
+        yield 'string_array' => [
             'string_array',
             [1, 2, 3, true, false],
             ['1', '2', '3', '1', ''],
         ];
 
-        yield [
+        yield 'bool_array with keys' => [
             'bool_array',
             ['t' => 1, 'f' => 0],
             ['t' => true, 'f' => false],
@@ -92,6 +104,39 @@ final class ArrayCastEnvVarProcessorTest extends TestCase
             'float_array',
             ['0.0', '1.0', '1.0.1'],
             'Non-numeric member of env var "%s" cannot be cast to float.',
+        ];
+    }
+
+    /**
+     * @dataProvider invalidBase64Provider
+     */
+    public function testInvalidBase64(string $prefix, array $envValue, string $expectedMessageFormat): void
+    {
+        $processor = new ArrayCastEnvVarProcessor();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage(sprintf($expectedMessageFormat, 'DM'));
+
+        $processor->getEnv($prefix, 'DM', static function () use ($envValue): array {
+            return $envValue;
+        });
+    }
+
+    /**
+     * @return \Generator<array{string, array, string}>
+     */
+    public function invalidBase64Provider(): \Generator
+    {
+        yield [
+            'base64_array',
+            ['Z!==', 'Z!'],
+            'Env var "%s" must be a valid base64 string.',
+        ];
+
+        yield [
+            'base64url_array',
+            ['-=', '_!'],
+            'Env var "%s" must be a valid base64url string.',
         ];
     }
 }
